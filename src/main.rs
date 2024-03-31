0d30c3d9ea77beda16d19149b8720608378dd029
Philipp Flenker <philipp.flenker@zalando.de>
2024-03-31 21:54:52 +0200

    Enter raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io::{self, Read};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 fn main() {
+    enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{}", c);
         if c == 'q' {
+            disable_raw_mode().unwrap();
             break;
         }
     }
