fc28b24fb72bb669fa74b86001c661864e3da36f
Philipp Flenker <hello@philippflenker.com>
2024-04-02 08:17:41 +0200

    Idiomatic Crossterm

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,6 @@
-use std::io::{self, Read};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
 
 pub struct Editor {
 
@@ -11,20 +12,21 @@ impl Editor {
     }
     pub fn run(&self){
         enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
-                    if c == 'q' {
-                        break;
-                    }
-                }
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match (event.code) {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        },
+                        _ => (),   
+                    }   
+                },
                 Err(err) => println!("Error: {}", err),
+                _ => ()
             }
         }
         disable_raw_mode().unwrap(); 
