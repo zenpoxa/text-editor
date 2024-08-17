82d9b23ea038108c15a3f8f66e466b8d82eef518
zenpoxa <zenpoxa@gmail.com>
2024-08-17 14:31:31 +0200

    chapitre 3 fini + tests sur début d'écriture au clavier dans l'éditeur

--- src/editor.rs
+++ src/editor.rs
@@ -1,82 +1,173 @@
-// Saisie utilisateur
-use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use core::cmp::min;
+use crossterm::event::{
+    read,
+    Event::{self, Key},
+    KeyCode, KeyEvent, KeyEventKind, KeyModifiers,
+};
 use std::io::Error;
-
 mod terminal;
-use terminal::{Terminal, Size, Position};
+use terminal::{Position, Size, Terminal};
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Copy, Clone, Default)]
+struct Location {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Default)]
 pub struct Editor {
-    should_quit : bool
+    should_quit: bool,
+    location: Location,
 }
 
 impl Editor {
-
-    pub const fn default() -> Self {
-        Self { should_quit : false }
-    }
-
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
         let result = self.repl();
         Terminal::terminate().unwrap();
-        result.unwrap(); // Peut importe s'il y a une erreur, on la vérifie à la fin (d'abord, faire initialize & terminate)
+        result.unwrap();
     }
 
     fn repl(&mut self) -> Result<(), Error> {
-
         loop {
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
-
             let event = read()?;
-            self.evaluate_event(&event);
+            self.evaluate_event(&event)?;
         }
-
         Ok(())
     }
-
-    // Check if we must quit
-    fn evaluate_event(&mut self, event : &Event) {
+    fn move_point(&mut self, key_code: KeyCode) -> Result<(), Error> {
+        let Location { mut x, mut y } = self.location;
+        let Size { height, width } = Terminal::size()?;
+        match key_code {
+            KeyCode::Up => {
+                y = y.saturating_sub(1);
+            }
+            KeyCode::Down => {
+                y = min(height.saturating_sub(1), y.saturating_add(1));
+            }
+            KeyCode::Left => {
+                x = x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                x = min(width.saturating_sub(1), x.saturating_add(1));
+            }
+            KeyCode::PageUp => {
+                y = 0;
+            }
+            KeyCode::PageDown => {
+                y = height.saturating_sub(1);
+            }
+            KeyCode::Home => {
+                x = 0;
+            }
+            KeyCode::End => {
+                x = width.saturating_sub(1);
+            }
+            _ => (),
+        }
+        self.location = Location { x, y };
+        Ok(())
+    }
+    fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
         if let Key(KeyEvent {
-            code: Char('w'),
-            modifiers: KeyModifiers::CONTROL,
+            code,
+            modifiers,
+            kind: KeyEventKind::Press,
             ..
         }) = event
         {
-            self.should_quit = true;
+            match code {
+                KeyCode::Char('w') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                KeyCode::Up
+                | KeyCode::Down
+                | KeyCode::Left
+                | KeyCode::Right
+                | KeyCode::PageDown
+                | KeyCode::PageUp
+                | KeyCode::End
+                | KeyCode::Home => {
+                    self.move_point(*code)?;
+                }
+                KeyCode::Char(my_char) if *modifiers == KeyModifiers::NONE => {
+                    self.write_char(my_char)?;
+                }
+                _ => (),
+            }
         }
+        Ok(())
     }
-
-    fn refresh_screen(&self) -> Result<(), Error> {
-        Terminal::hide_cursor()?;
+    fn write_char(&mut self, char: &char) -> Result<(), Error> {
+        Terminal::print(*char)?;
         
+        let Location { mut x, y } = self.location;
+        let Size { height, width } = Terminal::size()?;
+        x = min(width.saturating_sub(1), x.saturating_add(1));
+        self.location = Location { x, y };
+        Ok(())
+    }
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_caret()?;
+        Terminal::move_caret_to(Position::default())?;
         if self.should_quit {
             Terminal::clear_screen()?;
-            Terminal::print("Goodbye !")?;
-        }
-        else {
+            Terminal::print("Goodbye.\r\n")?;
+        } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(Position{x: 0, y: 0})?;
+            // revenir à la position actuelle
+            Terminal::move_caret_to(Position {
+                col: self.location.x,
+                row: self.location.y,
+            })?;
         }
 
-        Terminal::show_cursor()?;
+        Terminal::show_caret()?;
         Terminal::execute()?;
         Ok(())
     }
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+        let width = Terminal::size()?.width;
+        let len = welcome_message.len();
+        // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+        // it's allowed to be a bit to the left or right.
+        #[allow(clippy::integer_division)]
+        let padding = (width.saturating_sub(len)) / 2;
 
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
     fn draw_rows() -> Result<(), Error> {
-        let Size{height, ..} = Terminal::size()?;
-
+        let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
-            Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if current_row + 1 < height {
+            // Terminal::clear_line()?;
+            // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+            // it's allowed to be a bit up or down
+            #[allow(clippy::integer_division)]
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
-
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::all, clippy::pedantic, clippy::print_stdout)]
+#![warn(clippy::all, clippy::pedantic, clippy::print_stdout, clippy::arithmetic_side_effects, clippy::as_conversions, clippy::integer_division)]
 
 // Charger un module
 mod editor;
