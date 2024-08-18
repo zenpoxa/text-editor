e45fbe9384e9c662a69ffec103fefc8afcacd697
zenpoxa <zenpoxa@gmail.com>
2024-08-18 17:40:20 +0200

    début chap 4

--- src/editor.rs
+++ src/editor.rs
@@ -6,10 +6,9 @@ use crossterm::event::{
 };
 use std::io::Error;
 mod terminal;
+mod view;
 use terminal::{Position, Size, Terminal};
-
-const NAME: &str = env!("CARGO_PKG_NAME");
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+use view::View;
 
 #[derive(Copy, Clone, Default)]
 struct Location {
@@ -97,21 +96,9 @@ impl Editor {
                 | KeyCode::Home => {
                     self.move_point(*code)?;
                 }
-                KeyCode::Char(my_char) if *modifiers == KeyModifiers::NONE => {
-                    self.write_char(my_char)?;
-                }
                 _ => (),
             }
         }
-        Ok(())
-    }
-    fn write_char(&mut self, char: &char) -> Result<(), Error> {
-        Terminal::print(*char)?;
-        
-        let Location { mut x, y } = self.location;
-        let Size { height, width } = Terminal::size()?;
-        x = min(width.saturating_sub(1), x.saturating_add(1));
-        self.location = Location { x, y };
         Ok(())
     }
     fn refresh_screen(&self) -> Result<(), Error> {
@@ -121,8 +108,7 @@ impl Editor {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n")?;
         } else {
-            Self::draw_rows()?;
-            // revenir à la position actuelle
+            View::render()?;
             Terminal::move_caret_to(Position {
                 col: self.location.x,
                 row: self.location.y,
@@ -131,43 +117,6 @@ impl Editor {
 
         Terminal::show_caret()?;
         Terminal::execute()?;
-        Ok(())
-    }
-    fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
-        let width = Terminal::size()?.width;
-        let len = welcome_message.len();
-        // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
-        // it's allowed to be a bit to the left or right.
-        #[allow(clippy::integer_division)]
-        let padding = (width.saturating_sub(len)) / 2;
-
-        let spaces = " ".repeat(padding.saturating_sub(1));
-        welcome_message = format!("~{spaces}{welcome_message}");
-        welcome_message.truncate(width);
-        Terminal::print(welcome_message)?;
-        Ok(())
-    }
-    fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
-    }
-    fn draw_rows() -> Result<(), Error> {
-        let Size { height, .. } = Terminal::size()?;
-        for current_row in 0..height {
-            // Terminal::clear_line()?;
-            // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
-            // it's allowed to be a bit up or down
-            #[allow(clippy::integer_division)]
-            if current_row == height / 3 {
-                Self::draw_welcome_message()?;
-            } else {
-                Self::draw_empty_row()?;
-            }
-            if current_row.saturating_add(1) < height {
-                Terminal::print("\r\n")?;
-            }
-        }
         Ok(())
     }
 }
